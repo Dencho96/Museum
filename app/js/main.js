885fe4f3b157ce2a0cf61433ca9a0e6c05a616c8
Dencho96 <denysvk.96@gmail.com>
2020-11-09 22:53:54 +0200

    added all main sections/footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,38 @@
-var mySwiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper('.hero__slider', {
     slidesPerView: 1,
     loop: 'true',
     navigation: {
         nextEl: '.hero__btn--next',
         prevEl: '.hero__btn--prev',
       },
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-      },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    autoplay: {
+      delay: 3000,
+    },
+});
+
+var photogallery = new Swiper('.photogallery__slider', {
+  slidesPerView: 'auto',
+  loop: 'true',
+  spaceBetween: 10,
+  autoplay: {
+    delay: 3000,
+  },
+  navigation: {
+    nextEl: '.photogallery__btn--next',
+    prevEl: '.photogallery__btn--prev',
+  },
+});
+
+var photogallery = new Swiper('.partners__slider', {
+  slidesPerView: 5,
+  loop: 'true',
+  spaceBetween: 69,
+  navigation: {
+    nextEl: '.partners__btn--next',
+    prevEl: '.partners__btn--prev',
+  },
 });
